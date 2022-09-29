1ad0cec434967d3f36ae0425f208a03ef4ac3eed
Olga Rybnikova <olga.rybnikova777817@icloud.com>
2022-09-29 23:45:57 +0300

    hw selenium refactoring

--- src/main/java/com/example/selenium/steps/UserSteps.java
+++ src/main/java/com/example/selenium/steps/UserSteps.java
@@ -1,23 +1,30 @@
 package com.example.selenium.steps;
 
+import com.example.selenium.pages.LoginPage;
+import com.example.selenium.pages.MainPage;
 import org.openqa.selenium.WebDriver;
+
+import static org.testng.Assert.assertTrue;
 
 public class UserSteps {
     private WebDriver driver;
+    private MainPage mainPage;
 
     public UserSteps(WebDriver driver) {
         this.driver = driver;
     }
 
     public com.example.selenium.steps.UserSteps login(String username, String password) {
-        // TODO initialize LoginPage, call setUsername, setPassword, and click login methods
-
-        //  TODO assert title
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.setUsername(username);
+        loginPage.setPassword(password);
+        mainPage = loginPage.clickLogin();
+        assertTrue(mainPage.getTitle().contains("Secure Area"), "Login failed");
         return this;
     }
 
     public UserSteps logout() {
-        // TODO call logout method
+        mainPage.clickLogout();
         return this;
     }
 }
